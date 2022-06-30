dda54f1a04d7c066d7b1d5b710a668c523a69029
Fernando Padilla <fernandoslort@gmail.com>
2022-06-30 15:37:38 -0400

    update:src/modules/driver/infrastructure/driver.infrastructure.ts

--- src/modules/driver/infrastructure/driver.infrastructure.ts
+++ src/modules/driver/infrastructure/driver.infrastructure.ts
@@ -1,4 +1,6 @@
+import DataBase from '../../../services/database.services';
 import BaseInfrastructure from '../../../shared/infrastructure/base.infrastructure';
+import { DriverEntity } from '../domain/models/driver.entity';
 import DriverModel from '../domain/models/driver.model';
 import { DriverRepository } from '../domain/repositories/driver.repository';
 
@@ -8,5 +10,12 @@ export default class DriverInfrastructure
 {
   async findAll(): Promise<DriverModel[]> {
     throw new Error('Method not implemented.');
+  }
+
+  async create(driver: DriverModel): Promise<DriverModel> {
+    const dataSource = DataBase.dataSource();
+    const repository = dataSource.getRepository(DriverEntity);
+
+    return repository.save(driver);
   }
 }
