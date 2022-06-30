6a3d21599549cf1f0d50f336af8a76aa5ea60764
Fernando Padilla <fernandoslort@gmail.com>
2022-06-30 15:37:20 -0400

    update: src/modules/driver/application/driver.application.ts

--- src/modules/driver/application/driver.application.ts
+++ src/modules/driver/application/driver.application.ts
@@ -4,27 +4,27 @@ import DriverModel from '../domain/models/driver.model';
 export default class DriverApplication {
   constructor(private driverRepository: BaseRepository<DriverModel>) {}
 
-  async findAll(): Promise<DriverModel[]> {
-    return await this.driverRepository.findAll();
+  findAll(): Promise<DriverModel[]> {
+    return this.driverRepository.findAll();
   }
 
-  async findById(id: string): Promise<DriverModel> {
-    return await this.driverRepository.findById(id);
+  findById(id: string): Promise<DriverModel> {
+    return this.driverRepository.findById(id);
   }
 
-  async findByEmail(email: string): Promise<DriverModel> {
-    return await this.driverRepository.findByEmail(email);
+  findByEmail(email: string): Promise<DriverModel> {
+    return this.driverRepository.findByEmail(email);
   }
 
-  async create(driver: DriverModel): Promise<DriverModel> {
-    return await this.driverRepository.create(driver);
+  create(driver: DriverModel): Promise<DriverModel> {
+    return this.driverRepository.create(driver);
   }
 
-  async update(driver: DriverModel): Promise<DriverModel> {
-    return await this.driverRepository.update(driver);
+  update(driver: DriverModel): Promise<DriverModel> {
+    return this.driverRepository.update(driver);
   }
 
-  async delete(id: string): Promise<void> {
-    return await this.driverRepository.delete(id);
+  delete(id: string): Promise<void> {
+    return this.driverRepository.delete(id);
   }
 }
