607e5adc94abda05bb124e0a0d40b9ef3e5b184a
Fernando Padilla <fernandoslort@gmail.com>
2022-06-30 21:34:24 -0400

    update:src/modules/user/users.routes.ts

--- src/modules/user/users.routes.ts
+++ src/modules/user/users.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import UserApplication from './application/user.application';
 import UserInfrastructure from './infrastructure/user.infrastructure';
+import UserController from './user.controller';
 
 const infrastructure = new UserInfrastructure();
 const application = new UserApplication(infrastructure);
+const controller = new UserController(application);
 
 class Routes {
   router = express.Router();
