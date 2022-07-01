6bf9df58e1d7eb76d4a4796933835697e37393b9
Fernando Padilla <fernandoslort@gmail.com>
2022-06-30 21:34:08 -0400

    update:src/modules/user/user.controller.ts

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,11 +2,4 @@ import UserApplication from './application/user.application';
 
 export default class UserController {
   constructor(private userApplication: UserApplication) {}
-
-  // findAll(): Promise<T[]>;
-  // findById(id: string): Promise<T>;
-  // findByEmail(email: string): Promise<T>;
-  // create(user: T): Promise<T>;
-  // update(user: T): Promise<T>;
-  // delete(id: string): Promise<void>;
 }
