7079b46deebb7b0af73656bd2dd46bbe56506393
Fernando Padilla <fernandoslort@gmail.com>
2022-07-01 09:53:51 -0400

    update:src/modules/driver/driver.controller.ts

--- src/modules/driver/driver.controller.ts
+++ src/modules/driver/driver.controller.ts
@@ -9,7 +9,22 @@ export default class DriverController {
     try {
       const driver = new DriverFactory().create(req.body);
 
-      return this.driverApplication.create(driver);
+      const result = this.driverApplication.create(driver);
+
+      res.json(result);
+    } catch (error) {
+      res.json(error);
+    }
+  }
+
+  async findAll(_req: Request, res: Response) {
+    try {
+      const result = await this.driverApplication.findAll(
+        { isActive: true },
+        {},
+        []
+      );
+      res.json(result);
     } catch (error) {
       res.json(error);
     }
