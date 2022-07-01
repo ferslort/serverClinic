a2e8a25bbdb2d15e3809a3181ae8164de6226ca7
Fernando Padilla <fernandoslort@gmail.com>
2022-06-30 21:34:33 -0400

    update:src/shared/domain/repositories/baseRepository.ts

--- src/shared/domain/repositories/baseRepository.ts
+++ src/shared/domain/repositories/baseRepository.ts
@@ -1,8 +1,28 @@
+import Result from '../../application/interface/result.interface';
+
 export default interface BaseRepository<T> {
-  findAll(): Promise<T[]>;
-  findById(id: string): Promise<T>;
-  findByEmail(email: string): Promise<T>;
-  create(user: T): Promise<T>;
-  update(user: T): Promise<T>;
-  delete(id: string): Promise<void>;
+  findAll(
+    where: { [key: string]: string | number | boolean },
+    order: { [key: string]: string },
+    relations: string[]
+  ): Promise<Result<T>>;
+  findOne(where: {
+    [key: string]: string | number | boolean;
+  }): Promise<Result<T>>;
+  create(user: T): Promise<Result<T>>;
+  update(
+    where: { [key: string]: string | number | boolean },
+    order: { [key: string]: string },
+    relations: string[]
+  ): Promise<Result<T>>;
+  delete(where: {
+    [key: string]: string | number | boolean;
+  }): Promise<Result<T>>;
+  getPage(
+    page: number,
+    pageSize: number,
+    where: { [key: string]: string | number | boolean },
+    order: { [key: string]: string },
+    relations: string[]
+  ): Promise<Result<T>>;
 }
