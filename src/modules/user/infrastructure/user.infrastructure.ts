ec11fa7bf8d3f9f37a3de0887e3e3df9c2d9c5c6
Fernando Padilla <fernandoslort@gmail.com>
2022-06-30 21:34:00 -0400

    update:src/modules/user/infrastructure/user.infrastructure.ts

--- src/modules/user/infrastructure/user.infrastructure.ts
+++ src/modules/user/infrastructure/user.infrastructure.ts
@@ -6,27 +6,7 @@ export default class UserInfrastructure
   extends BaseInfrastructure<UserModel, string>
   implements UserRepository
 {
-  create(user: UserModel): Promise<UserModel> {
-    throw new Error('Method not implemented.');
-  }
-
-  delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  findAll(): Promise<UserModel[]> {
-    throw new Error('Method not implemented.');
-  }
-
-  findById(id: string): Promise<UserModel> {
-    throw new Error('Method not implemented.');
-  }
-
-  findByEmail(email: string): Promise<UserModel> {
-    throw new Error('Method not implemented.');
-  }
-
-  update(user: UserModel): Promise<UserModel> {
-    throw new Error('Method not implemented.');
+  constructor() {
+    super(UserModel);
   }
 }
